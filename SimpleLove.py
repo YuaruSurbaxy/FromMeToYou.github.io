f8b803a0bce473007252656bb8f87d0a09fd3bde
YuaruSurbaxy <testi19774@gmail.com>
2024-08-28 21:53:09 +0900

    SimpleLove.py

---
+++ SimpleLove.py
@@ -0,0 +1,16 @@
+import time 
+from turtle import*
+
+
+bgcolor("pink")
+color("red")
+begin_fill()
+pensize(3)
+left(50)
+forward(133)
+circle(50, 200)
+right(140)
+circle(50, 200)
+forward(133)
+end_fill()
+time.sleep(10)
